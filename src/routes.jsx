29f8b639cce2e6d0fcc177b668bd40899f67f698
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-23 15:16:12 +0100

    ajout route pour page not found

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,14 +4,16 @@ import Home from './pages/home/Home'
 import Profil from './pages/profilUser/ProfilUser'
 import Settings from './pages/settings/Settings'
 import Community from './pages/community/Community'
+import NotFound from './pages/notFound/NotFound'
 
 function RoutesApp() {
     return (
         <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/user/:userId" element={<Profil />}></Route>
-            <Route path="/Settings" element={<Settings />}></Route>
-            <Route path="/Community" element={<Community />}></Route>
+            <Route path="/settings" element={<Settings />}></Route>
+            <Route path="/community" element={<Community />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
         </Routes>
     )
 }

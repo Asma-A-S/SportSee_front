ddde75e5ba2c69fd97bc3c59e9cf0b680491b95b
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-23 11:27:41 +0100

    création des pages, profil, accueil, paramètres et communité. sans implémentation

---
+++ src/pages/community/Community.jsx
@@ -0,0 +1,4 @@
+function Community() {
+    return <h1>Community page</h1>
+}
+export default Community
---
+++ src/pages/profilUser/ProfilUser.jsx
@@ -0,0 +1,4 @@
+function Profil() {
+    return <h1>profil page</h1>
+}
+export default Profil

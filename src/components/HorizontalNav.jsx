2b7246ea45e51c510426b22e95c49aa29e6cf48b
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-23 15:12:50 +0100

    envelopper les navlink dans balise ul

--- src/components/HorizontalNav.jsx
+++ src/components/HorizontalNav.jsx
@@ -1,22 +1,33 @@
 import logo from '../assets/logo.png'
 import { NavLink } from 'react-router-dom'
 function HorizontalNav() {
+    const id = 12
     return (
         <div className="header">
             <img src={logo} alt="logo" className="header_logo" />
             <nav className="header_nav">
-                <NavLink to="/" className="header_nav_item">
-                    Accueil
-                </NavLink>
-                <NavLink to="/user/:userId" className="header_nav_item">
-                    Profil
-                </NavLink>
-                <NavLink to="/Settings" className="header_nav_item">
-                    Réglages
-                </NavLink>
-                <NavLink to="/Community" className="header_nav_item">
-                    Communité
-                </NavLink>
+                <ul>
+                    <li>
+                        <NavLink to="/" className="header_nav_item">
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/user/${id}`} className="header_nav_item">
+                            Profil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/Settings" className="header_nav_item">
+                            Réglages
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/Community" className="header_nav_item">
+                            Communauté
+                        </NavLink>
+                    </li>
+                </ul>
             </nav>
         </div>
     )

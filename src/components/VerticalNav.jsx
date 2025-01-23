80f894d06971efbc9816cd4fa65f45f65827fb0b
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-01-23 15:15:10 +0100

    remplacer la div par nav et ajout classname

--- src/components/VerticalNav.jsx
+++ src/components/VerticalNav.jsx
@@ -3,6 +3,33 @@ import swim from '../assets/swim.png'
 import velo from '../assets/velo.png'
 import build from '../assets/build.png'
 
-import { Link } from 'react-dom'
-function VerticalNav() {}
+function VerticalNav() {
+    return (
+        <div className="vertical-nav">
+            <nav className="vertical-nav_nav">
+                <img
+                    src={yoga}
+                    alt="icone yoga"
+                    className="vertical-nav_icone"
+                />
+                <img
+                    src={swim}
+                    alt="swim icone"
+                    className="vertical-nav_icone"
+                />
+                <img
+                    src={velo}
+                    alt="velo icone"
+                    className="vertical-nav_icone"
+                />
+                <img
+                    src={build}
+                    alt="body build icone"
+                    className="vertical-nav_icone"
+                />
+            </nav>
+            <p className="vertical-nav_copyright">Copyright, SportSee 2020</p>
+        </div>
+    )
+}
 export default VerticalNav

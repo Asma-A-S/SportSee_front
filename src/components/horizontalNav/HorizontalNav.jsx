c8d99a7a75e54b8bfb565ca2ea301954705e4645
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-02-21 16:06:52 +0100

    ajout composant horizontal nav et son style

---
+++ src/components/horizontalNav/HorizontalNav.jsx
@@ -0,0 +1,36 @@
+import logo from '../../assets/logo.png'
+import { NavLink } from 'react-router-dom'
+import '../horizontalNav/horizontalNav.scss'
+function HorizontalNav() {
+    const id = 12
+    return (
+        <div className="header">
+            <img src={logo} alt="logo" className="header_logo" />
+            <nav>
+                <ul className="header_nav">
+                    <li>
+                        <NavLink to="/" className="header_nav_item">
+                            Accueil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/user/${id}`} className="header_nav_item">
+                            Profil
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/Settings" className="header_nav_item">
+                            Réglages
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/Community" className="header_nav_item">
+                            Communauté
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default HorizontalNav

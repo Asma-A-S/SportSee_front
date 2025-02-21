2b55a30c7190b60a81fcc1e784f2540111ae4739
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-02-21 15:58:07 +0100

    implémenter la page utilisateur

--- src/pages/profilUser/ProfilUser.jsx
+++ src/pages/profilUser/ProfilUser.jsx
@@ -1,4 +1,40 @@
+import React from 'react'
+import { UserProvider } from '../../services/UserProvider'
+import NutrientCards from '../../components/NutrientCards'
+import Activity from '../../components/Activity'
+import AverageSessions from '../../components/AverageSessions'
+import Performance from '../../components/Performance'
+import Score from '../../components/Score'
+import MainData from '../../components/MainData'
+
+//importer icones
+import caloriesIcone from '../../assets/calories-icon.png'
+import proteines from '../../assets/protein-icon.png'
+import glucides from '../../assets/carbs-icon.png'
+import lipids from '../../assets/fat-icon.png'
+
 function Profil() {
-    return <h1>profil page</h1>
+    return (
+        <UserProvider>
+            <MainData />
+            <section className="main-graphiques">
+                <div className="nutrients">
+                    <NutrientCards icone={caloriesIcone} name="Calories" />
+                    <NutrientCards icone={proteines} name="Proteines" />
+                    <NutrientCards icone={glucides} name="Glucides" />
+                    <NutrientCards icone={lipids} name="Lipides" />
+                </div>
+                <div className="graphiques">
+                    <Activity />
+                    <div className="graphiques_perf">
+                        <AverageSessions />
+                        <Performance />
+                        <Score />
+                    </div>
+                </div>
+            </section>
+        </UserProvider>
+    )
 }
+
 export default Profil

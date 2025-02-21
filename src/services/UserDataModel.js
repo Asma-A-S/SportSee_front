0b08ef55d47b55918b42434de5d205c5a52083a8
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-02-21 15:55:53 +0100

    ajout classes modélisation des données récupérées

---
+++ src/services/UserDataModel.js
@@ -0,0 +1,63 @@
+/**
+ *
+ */
+export class UserMainData {
+    constructor(data) {
+        this.userId = data.id
+        this.firstName = data.userInfos.firstName
+        this.score = data.todayScore || data.score
+        this.calories = data.keyData.calorieCount
+        this.proteines = data.keyData.proteinCount
+        this.glucides = data.keyData.carbohydrateCount
+        this.lipides = data.keyData.lipidCount
+    }
+}
+
+export class UserActivity {
+    constructor(data) {
+        this.userId = data.userId
+        this.sessions = data.sessions.map((session) => ({
+            day: new Date(session.day).getDate().toString(),
+            kilogram: session.kilogram,
+            calories: session.calories,
+        }))
+    }
+}
+
+export class UserAverageSessions {
+    constructor(data) {
+        this.userId = data.userId
+        this.sessions = data.sessions.map((session) => ({
+            day: this.getDayLetter(session.day),
+            sessionLength: session.sessionLength,
+        }))
+    }
+    getDayLetter(day) {
+        const days = ['L', 'M', 'M', 'J', 'V', 'S', 'D']
+        return days[day - 1] || ''
+    }
+}
+
+export class UserPerformance {
+    constructor(data) {
+        this.userId = data.userId
+        this.frenchKinds = {
+            1: 'Cardio',
+            2: 'Énergie',
+            3: 'Endurance',
+            4: 'Force',
+            5: 'Vitesse',
+            6: 'Intensité',
+        }
+        this.data = data.data
+            .map((item) => ({
+                kind: this.getKind(item.kind),
+                value: item.value,
+            }))
+            .reverse()
+    }
+
+    getKind(frenchKind) {
+        return this.frenchKinds[frenchKind] || ''
+    }
+}

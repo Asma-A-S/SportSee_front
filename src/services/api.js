b81de6fea435d754f3b60be566d3e776c4ad4632
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-02-21 15:50:01 +0100

    ajout docuemnation jsdoc, correction url,

--- src/services/api.js
+++ src/services/api.js
@@ -5,33 +5,58 @@ const api = axios.create({
     baseURL: base_URL,
 })
 
-const axiosData = async (url, mockKey) => {
+/**
+ * Récupérer les données de l'utilisateur depuis l'API ou le mock, selon l'environnement
+ * @param {string} url - l'url de l'api
+ * @param {string} mockKey - la clé des données mockées
+ * @param {number} userId - l'ID de l'utilisateur
+ * @returns {Promise<Object>} les données de l'utilisateur
+ */
+const axiosData = async (url, mockKey, userId) => {
     if (process.env.NODE_ENV === 'development') {
-        console.log(mockData[mockKey])
-        return mockData[mockKey]
+        const data = mockData[mockKey]
+        const userIdNumber = parseInt(userId, 10)
+        const result = data.find(
+            (user) => user.id === userIdNumber || user.userId === userIdNumber
+        )
+        return result
     }
     const response = await api.get(url)
     return response.data.data
 }
+/**
+ *
+ * @param {number} userId
+ * @returns {Promise<Object>} les informations de l'utilisateur
+ */
 export const getUserMainData = async (userId) => {
-    console.log('info')
-    return axiosData(`/user/${userId}`, 'USER_MAIN_DATA')
+    return axiosData(`/user/${userId}`, 'USER_MAIN_DATA', userId)
 }
-
+/**
+ *
+ * @param {number} userId
+ * @returns {Promise<Object>} les données de l'activité de l'utilisateur
+ */
 export const getUserActivity = async (userId) => {
-    console.log('activité')
-    return axiosData(`/user/${userId}/Activity`, 'USER_ACTIVITY')
+    return axiosData(`/user/${userId}/activity`, 'USER_ACTIVITY', userId)
 }
-
+/**
+ *
+ * @param {number} userId
+ * @returns {Promise<Object>} les données des sessions de l'utilisateur
+ */
 export const getUserAverageSessions = async (userId) => {
-    console.log('SESSIONS')
     return axiosData(
-        `/user/${userId}/Average-sessions`,
-        'USER_AVERAGE_SESSIONS'
+        `/user/${userId}/average-sessions`,
+        'USER_AVERAGE_SESSIONS',
+        userId
     )
 }
-
+/**
+ *
+ * @param {number} userId
+ * @returns {Promise<Object>} les données de la performance de l'utilisateur
+ */
 export const getUserPerformance = async (userId) => {
-    console.log('performance')
-    return axiosData(`/user/${userId}/performance`, 'USER_PERFORMANCE')
+    return axiosData(`/user/${userId}/performance`, 'USER_PERFORMANCE', userId)
 }

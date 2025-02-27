2af1e7e00a06d88ace009c0cce462d461a4df395
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-02-27 22:40:58 +0100

    ajout appel api avec useEffectGetData, passer des props au composants

--- src/pages/profilUser/ProfilUser.jsx
+++ src/pages/profilUser/ProfilUser.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { UserProvider } from '../../services/UserProvider'
+import { useParams } from 'react-router-dom'
 import NutrientCards from '../../components/NutrientCards'
 import Activity from '../../components/Activity'
 import AverageSessions from '../../components/AverageSessions'
 import Performance from '../../components/Performance'
 import Score from '../../components/Score'
 import MainData from '../../components/MainData'
+import {
+    getUserMainData,
+    getUserActivity,
+    getUserAverageSessions,
+    getUserPerformance,
+} from '../../services/api'
+
+import useEffectGetData from '../../services/useEffectGetData'
+import {
+    UserMainData,
+    UserActivity,
+    UserAverageSessions,
+    UserPerformance,
+} from '../../services/UserDataModel'
 
 //importer icones
 import caloriesIcone from '../../assets/calories-icon.png'
@@ -22,26 +36,65 @@ import lipids from '../../assets/fat-icon.png'
  * @returns {JSX.Element} Le profil complet de l'utilisateur avec des informations et des graphiques associés.
  */
 function Profil() {
+    const { userId } = useParams()
+    const mainData = useEffectGetData(getUserMainData, userId, UserMainData)
+    const activityData = useEffectGetData(getUserActivity, userId, UserActivity)
+    const averageSessionsData = useEffectGetData(
+        getUserAverageSessions,
+        userId,
+        UserAverageSessions
+    )
+    const performanceData = useEffectGetData(
+        getUserPerformance,
+        userId,
+        UserPerformance
+    )
+    if (
+        mainData.loading ||
+        activityData.loading ||
+        averageSessionsData.loading ||
+        performanceData.loading
+    ) {
+        return <p>Chargement des données...</p>
+    }
     return (
-        <UserProvider>
-            <MainData />
+        <>
+            <MainData data={mainData.data.firstName} />
             <section className="main-graphiques">
                 <div className="nutrients">
-                    <NutrientCards icone={caloriesIcone} name="Calories" />
-                    <NutrientCards icone={proteines} name="Proteines" />
-                    <NutrientCards icone={glucides} name="Glucides" />
-                    <NutrientCards icone={lipids} name="Lipides" />
+                    <NutrientCards
+                        icone={caloriesIcone}
+                        name="Calories"
+                        data={mainData.data.calories}
+                    />
+                    <NutrientCards
+                        icone={proteines}
+                        name="Proteines"
+                        data={mainData.data.proteines}
+                    />
+                    <NutrientCards
+                        icone={glucides}
+                        name="Glucides"
+                        data={mainData.data.glucides}
+                    />
+                    <NutrientCards
+                        icone={lipids}
+                        name="Lipides"
+                        data={mainData.data.lipides}
+                    />
                 </div>
                 <div className="graphiques">
-                    <Activity />
+                    <Activity data={activityData.data.sessions} />
                     <div className="graphiques_perf">
-                        <AverageSessions />
-                        <Performance />
-                        <Score />
+                        <AverageSessions
+                            data={averageSessionsData.data.sessions}
+                        />
+                        <Performance data={performanceData.data.data} />
+                        <Score data={mainData.data.score} />
                     </div>
                 </div>
             </section>
-        </UserProvider>
+        </>
     )
 }
 
